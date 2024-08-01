f3960e748a22cb3bf1ba50f15f6e1c9095b2101a
RiddhiLokhande <riddhilokhande7@gmail.com>
2024-08-02 02:48:11 +0530

    Add files via upload

---
+++ EXP 3 B.cpp
@@ -0,0 +1,19 @@
+//RIDDHI LOKAHNDE
+//ENTC B2
+//23070123107
+//EXP 3 B
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int b=6;
+    
+    cout << "\nBitwise Operators:" << endl;
+    cout << "AND (a & b): " << (a & b) << endl;
+    cout << "OR (a | b): " << (a | b) << endl;
+    cout << "XOR (a ^ b):" << (a ^ b) << endl;
+    cout << "NOT (~a) : " << (~a) << endl;
+    cout << "LEFT SHIFT (a << 1):" << (a << 1) << endl;
+    cout << "RIGHT SHIFT (a >> 1) : " << (a >> 1) << endl;
+    return 0;
+}
